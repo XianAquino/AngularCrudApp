f2203abb08ee4f3f917e3c352ba4a6c1b3611093
XianAquino <xian_a@yahoo.com>
2017-05-01 14:30:41 -0700

    get all managers and subordinates

--- server/server.js
+++ server/server.js
@@ -2,6 +2,27 @@ var express = require('express');
 var path = require('path');
 var app = express();
 var port = 3033;
+var async = require('async');
+var employees = require('./models/employees.js');
+
+var getEmployees = function(callback) {
+  employees.getAll(null, function(err, managers) {
+      async.forEach(managers, function(manager, callback) {
+        employees.getAll(manager.ID, function(err, subordinates) {
+            manager.subordinates = subordinates;
+            callback();
+        });
+      }, function(err) {
+        callback(managers);
+      });
+  });
+}
+
+app.get('/employees', function(req, res) {
+  getEmployees(function(employees) {
+    res.json(employees);
+  })
+});
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname +  '/../client/index.html'));
